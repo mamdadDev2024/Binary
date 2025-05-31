9cec986ef575981c1442eed5ecc4e0ecfad683c9
Mohammad Hosein <madmanprogrammer2024@gmail.com>
2025-05-31 15:52:09 +0330

    add unit tests , and readme.md

---
+++ tests/Units/EndianessTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Units;
+
+use Nobody\BinaryTool\Enums\Byte;
+use Nobody\BinaryTool\Enums\Endian;
+use Nobody\BinaryTool\Facades\Binary;
+use PHPUnit\Framework\TestCase;
+
+class Endianess extends TestCase
+{
+    public function testBigEndian()
+    {
+        $value = 0x1234;
+        $packed = Binary::pack($value, Byte::EIGHT, Endian::BIG, false);
+        $this->assertEquals("\x12\x34", $packed);
+    }
+
+    public function testLittleEndian()
+    {
+        $value = 0x1234;
+        $packed = Binary::pack($value, Byte::EIGHT, Endian::BIG, false);
+        $this->assertEquals("\x34\x12", $packed);
+    }
+
+}
